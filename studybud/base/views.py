cf99d33151de218148f1d06d538e36df52f9f728
Wendler Juan Jose <juanjoowendler@gmail.com>
2025-12-02 17:00:34 -0300

    templates

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Home page")
+rooms = [
+    {"id": 1, "name": "Study Room 1"},
+    {"id": 2, "name": "Study Room 2"},
+    {"id": 3, "name": "Study Room 3"},
+]
 
-def room(request):
-    return HttpResponse("Room page")
+def home(request):
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
+
+def room(request, pk):
+    room = None
+    for r in rooms: 
+        if r["id"] == int(pk):
+            room = r
+
+    context = {"room": room}
+    return render(request, "base/room.html", context)
--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = "studybud.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ # My templates here
+            BASE_DIR / "templates"
+            ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
